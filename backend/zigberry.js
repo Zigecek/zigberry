bedbbce04f25b58783a47322328488bfd7f2ff9c
Jan Kozohorský <jan.kozohorsky@infis.cz>
2022-01-16 04:47:33 +0100

    Update zigberry.js

--- backend/zigberry.js
+++ backend/zigberry.js
@@ -50,7 +50,7 @@ app.post("/login", async (req, res) => {
       } else {
         if (user.password == password) {
           req.session.user = username;
-          res.redirect("/dash");
+          return res.redirect("/dash");
         } else {
           return res.status(410).send("Špatné heslo");
         }

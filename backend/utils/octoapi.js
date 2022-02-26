a542b813c3c824143fdc4e6fe8e6302c42361cfc
Jan Kozohorský <honza007cz@hotmail.com>
2022-02-26 22:43:11 +0100

    Update octoapi.js

--- backend/utils/octoapi.js
+++ backend/utils/octoapi.js
@@ -83,6 +83,7 @@ const fns = {
 
     console.log(uid);
     var res = await api("/api/printer?exclude=temperature,sd", "GET");
+    console.log(res.statusCode ? 409 : res);
     if (res) {
       console.log("1");
       if (

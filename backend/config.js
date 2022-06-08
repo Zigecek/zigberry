70fb788fc87fe640f102ee1dda63b77b2cb55f93
Jan Kozohorský <honza007cz@hotmail.com>
2022-06-08 23:03:11 +0200

    Bugfixes ...

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 module.export = {
   autoOffDelay: 30 * 60 * 1000, // 30 mins
   apiPath: "https://octo.kozohorsky.xyz",
-  webcamPath: "/webcam0/?action=stream",
+  webcamPath: "/webcam/?action=stream",
   port: 3388,
   cookieLifetime: 1000 * 60 * 60 * 24 * 3, // 3 days
 };
